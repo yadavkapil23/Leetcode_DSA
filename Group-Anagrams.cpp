14ab01581d61f56f21ed6186fcd1c99567af6d46
Kapil <kydv901@gmail.com>
2025-09-29 16:15:17 +0530

    [49] [Time Beats: 61.63%] [Memory Beats: 85.78%] - LeetPush

---
+++ Group-Anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+
+        for (auto &s : strs) {
+            string key = s;
+            sort(key.begin(), key.end()); 
+            mp[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        for (auto &p : mp) {
+            result.push_back(p.second);
+        }
+
+        return result;
+    }
+};
