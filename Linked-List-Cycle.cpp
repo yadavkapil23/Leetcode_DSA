e782cd32abbf415a6e6d78349c05f69e972e9938
Kapil <kydv901@gmail.com>
2025-11-04 13:53:56 +0530

    [141] [Time Beats: 38.05%] [Memory Beats: 52.49%] - LeetPush

---
+++ Linked-List-Cycle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == nullptr) return false;
+        
+        ListNode *slow = head;
+        ListNode *fast = head;
+        
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            
+            if (slow == fast) {
+                return true;  
+            }
+        }
+        return false;  
+    }
+};
