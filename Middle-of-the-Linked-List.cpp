7e83218d220199c44832bda791556abcd5452a24
Kapil <kydv901@gmail.com>
2025-11-09 23:37:48 +0530

    [876] [Time Beats: 100.00%] [Memory Beats: 25.27%] - LeetPush

---
+++ Middle-of-the-Linked-List.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+       ListNode* slow=head;
+       ListNode* fast=head;
+       ListNode* next = nullptr;
+       while(fast != nullptr && fast->next!=nullptr){
+        slow = slow->next;
+        fast = fast->next->next;
+       };
+       return slow; 
+    }
+};
