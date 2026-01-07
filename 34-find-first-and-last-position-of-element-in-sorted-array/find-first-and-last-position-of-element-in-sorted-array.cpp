07060ad3c4135afa49ee65c9faa25494c92ffe4c
Kapil <kydv901@gmail.com>
2026-01-07 21:21:11 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (16.63%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,20 +1,45 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& v, int target) {
+        //using sorting.
+        //sort(v.begin(),v.end());
         int n = v.size();
-        int start = -1;
-        int end = -1;
-        for(int i=0 ; i<n ; i++){
-            if(v[i]==target){
-                if(start == -1){
-                    start = i;
-                }
-                end = i;
+        int first=-1;
+        int last=-1;
+
+        int  low = 0;
+        int high = n-1;
+
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            if(v[mid] == target){
+                first = mid; // searched for first , now check whether any other first exists in left or not.
+                high = mid-1;
+            }
+            else if(v[mid] < target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
             }
         }
-            vector<int> result;
-            result.push_back(start);
-            result.push_back(end);
-        return result;
+
+            low = 0;
+            high = n-1;
+
+            while(low <= high){
+                int mid = low + (high-low)/2;
+                if(v[mid] == target){
+                    last = mid; // now check right to seeany other last exists or not.
+                    low = mid+1;
+                }
+                else if(v[mid] < target){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        return {first,last};
     }
 };
