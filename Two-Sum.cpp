12ec96e51867531c9c8d655d339ec16e3ea14602
Kapil <kydv901@gmail.com>
2026-02-24 22:21:53 +0530

    [1] [Time Beats: 45.26%] [Memory Beats: 36.00%] - LeetPush

--- Two-Sum.cpp
+++ Two-Sum.cpp
@@ -3,18 +3,28 @@
 3    vector<int> twoSum(vector<int>& v, int target) {
 4        int n = v.size();
 5
-6    vector<int> result;
+6    vector<pair<int,int>> result;
 7
-8    int sum = 0;
-9    for(int i=0 ; i<n ; i++){
-10        for(int j=i+1 ; j<n ; j++){
-11            sum = v[i] + v[j];
-12            if(sum==target){
-13                result.push_back(i);
-14                result.push_back(j);
-15            }
-16        }
-17    }
-18    return result;
-19    }
-20};
+8    for(int i=0 ; i<n ; i++){
+9        result.push_back({v[i],i});
+10    }
+11    int l = 0;
+12    int r = n-1;
+13
+14    sort(result.begin(),result.end());
+15
+16    while(l<r){
+17    int sum = result[l].first + result[r].first;
+18    if(sum == target){
+19        return {result[l].second,result[r].second};
+20    }
+21    else if(sum < target){
+22        l++;
+23    }
+24    else{
+25        r--;
+26    }
+27    }
+28    return {};
+29    }
+30};
