4e81bf8eb57032baac892e0872856ddc71fec058
Kapil <kydv901@gmail.com>
2026-02-25 17:20:29 +0530

    Time: 15 ms (5.44%) | Memory: 16.2 MB (5.22%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+
+map<int, string, greater<int>> mp;
+
+mp[1000] = "M";
+mp[900]  = "CM";
+mp[500]  = "D";
+mp[400]  = "CD";
+mp[100]  = "C";
+mp[90]   = "XC";
+mp[50]   = "L";
+mp[40]   = "XL";
+mp[10]   = "X";
+mp[9]    = "IX";
+mp[5]    = "V";
+mp[4]    = "IV";
+mp[1]    = "I";
+
+string ans;
+    
+for(auto &p : mp){
+    while(num>=p.first){
+        ans.append(p.second);
+        num = num-p.first;
+    }
+}
+return ans;
+    }
+};
