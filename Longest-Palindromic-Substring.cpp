520d216df73cec9fbe5a9f3083dbdba72ac69a9e
Kapil <kydv901@gmail.com>
2026-02-27 18:38:01 +0530

    [5] [Time Beats: 63.57%] [Memory Beats: 79.61%] - LeetPush

--- Longest-Palindromic-Substring.cpp
+++ Longest-Palindromic-Substring.cpp
@@ -1,34 +1,47 @@
 1class Solution {
 2public:
-3
-4    bool solve(string &s, int i, int j) {
-5    if (i >= j){
-6        return true;
-7    }
-8
-9    if (s[i] != s[j]){
-10        return false;
-11    }
-12
-13    return solve(s, i + 1, j - 1);
-14}
-15
-16    string longestPalindrome(string s) {
-17        int n = s.size();
-18
-19        int starting=0;
-20        int maxi = INT_MIN;
-21
-22        for(int i=0 ; i<n ; i++){
-23            for(int j=i ; j<n ; j++){
-24                if(solve(s,i,j)){
-25                    if((j-i+1) > maxi){
-26                        maxi = j-i+1;
-27                        starting = i;
-28                    }
-29                }
-30            }
-31        }
-32        return s.substr(starting,maxi);
-33    }
-34};
+3    string longestPalindrome(string s) {
+4        int start=0;
+5        int maxlen=1;
+6        int n = s.size();
+7        if(n==0){
+8            return "";
+9        }
+10        else if(n==1){
+11            return s;
+12        }
+13        else{
+14            //for even
+15            int l;
+16            int r;
+17            for(int i=0 ; i<n ; i++){
+18                l=i;
+19                r=i;
+20                while(l>=0 && r<n && s[l]==s[r]){
+21                    int len = r-l+1;
+22                    if(len > maxlen){
+23                        maxlen = len;
+24                        start = l;
+25                    }
+26                    l--;
+27                    r++;
+28                }
+29
+30                //for odd
+31                l=i;
+32                r=i+1;
+33                while(l>=0 && r<n && s[l]==s[r]){
+34                    int len = r-l+1;
+35                    if(len>maxlen){
+36                        maxlen = len;
+37                        start = l;
+38                    }
+39                    l--;
+40                    r++;
+41                }
+42            }
+43        }
+44
+45        return s.substr(start,maxlen);
+46    }
+47};
