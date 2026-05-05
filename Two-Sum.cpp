fc60dc9ef2012e430639687235c3d696fabff9a4
Kapil <kydv901@gmail.com>
2026-05-05 13:44:15 +0530

    [1] [Time Beats: 28.13%] [Memory Beats: 68.43%] - LeetPush

--- Two-Sum.cpp
+++ Two-Sum.cpp
@@ -1,20 +1,14 @@
 1class Solution {
 2public:
-3    vector<int> twoSum(vector<int>& v, int target) {
-4        int n = v.size();
-5
-6        unordered_map<int,int> m;
-7        int compliment;
-8
-9        for(int i=0 ; i<n ; i++){
-10        compliment = target - v[i];
-11
-12        if(m.find(compliment) != m.end()){
-13            return {m[compliment],i};
-14        }
-15
-16        m[v[i]] = i;
-17        }
-18        return {};
-19    }
-20};
+3    vector<int> twoSum(vector<int>& nums, int target) {
+4        int n = nums.size();
+5        for(int i=0 ; i<n ; i++){
+6            for(int j=i+1 ; j<n ; j++){
+7                if(nums[i] + nums[j] == target){
+8                    return {i,j};
+9                }
+10            }
+11        }
+12        return {};
+13    }
+14};
