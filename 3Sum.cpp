18ffbe180e2b05a88cbb331390debdb9d16b26c4
Kapil <kydv901@gmail.com>
2026-05-05 22:30:52 +0530

    [15] [Time Beats: 84.93%] [Memory Beats: 44.31%] - LeetPush

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,32 +1,44 @@
-class Solution {
-public:
-    vector<vector<int>> threeSum(vector<int>& v) {
-        vector<vector<int>> res;
-        sort(v.begin(), v.end());
-
-        for(int i = 0; i < v.size() - 2; i++) { 
-            if(i > 0 && v[i] == v[i-1]){
-                continue;
-            }
-            int left = i + 1;
-            int right = v.size() - 1;
-
-            while(left < right) {
-                int sum = v[i] + v[left] + v[right];
-                if(sum == 0) { 
-                    res.push_back({v[i], v[left], v[right]});
-
-                    while(left<right && v[left] == v[left+1]) left++;
-                    while(left<right && v[right] == v[right-1]) right--;
-
-                    left++;
-                    right--;
-                }
-                else if(sum < 0) left++;
-                else right--;
-            }
-        }
-
-        return res;
-    }
-};
+1class Solution {
+2public:
+3    vector<vector<int>> threeSum(vector<int>& nums) {
+4        sort(nums.begin(),nums.end());
+5        vector<vector<int>> ans;
+6        int sum = 0;
+7        int n = nums.size();
+8
+9        for(int i=0 ; i<n-2 ; i++){
+10            if(i>0 && nums[i] == nums[i-1]){
+11                continue;
+12            }
+13
+14        int left = i+1;
+15        int right = n-1;
+16
+17        while(left < right){
+18            sum = nums[i] + nums[left] + nums[right];
+19            if(sum == 0){
+20                ans.push_back({nums[i],nums[left],nums[right]});
+21                left++;
+22                right--;
+23
+24            while(left <  right && nums[left] == nums[left-1]){
+25                left++;
+26            }
+27
+28            while(left <  right && nums[right] == nums[right+1]){
+29                right--;
+30            }
+31            }
+32
+33           else if(sum<0){
+34                left++;
+35            }
+36
+37            else{
+38                right--;
+39            }
+40        }
+41        }
+42        return ans;
+43    }
+44};
