22280d67dd87519679fb326dbf05bfc5adc6c6f1
Kapil <kydv901@gmail.com>
2026-05-31 10:35:10 +0530

    [15] [Time Beats: 47.30%] [Memory Beats: 44.76%] - LeetPush

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -1,44 +1,41 @@
 1class Solution {
 2public:
 3    vector<vector<int>> threeSum(vector<int>& nums) {
-4        sort(nums.begin(),nums.end());
+4        int n = nums.size();
 5        vector<vector<int>> ans;
-6        int sum = 0;
-7        int n = nums.size();
-8
-9        for(int i=0 ; i<n-2 ; i++){
-10            if(i>0 && nums[i] == nums[i-1]){
-11                continue;
-12            }
-13
-14        int left = i+1;
-15        int right = n-1;
-16
-17        while(left < right){
-18            sum = nums[i] + nums[left] + nums[right];
-19            if(sum == 0){
-20                ans.push_back({nums[i],nums[left],nums[right]});
-21                left++;
-22                right--;
-23
-24            while(left <  right && nums[left] == nums[left-1]){
-25                left++;
-26            }
-27
-28            while(left <  right && nums[right] == nums[right+1]){
-29                right--;
-30            }
-31            }
-32
-33           else if(sum<0){
-34                left++;
-35            }
-36
-37            else{
-38                right--;
-39            }
-40        }
-41        }
-42        return ans;
-43    }
-44};
+6        sort(nums.begin(),nums.end());
+7
+8        for(int i=0 ; i<n-2 ; i++){
+9            if(i>0 && nums[i] == nums[i-1]){
+10                continue;
+11            }
+12
+13        int left = i+1;
+14        int right = n-1;
+15
+16        while(left < right){
+17            int sum = nums[i] + nums[left] + nums[right];
+18            if(sum == 0){
+19                ans.push_back({nums[i],nums[left],nums[right]});
+20                left++,right--;
+21
+22            while(left < right  && nums[left] == nums[left-1]){
+23                left++;
+24            }
+25
+26            while(left <  right && nums[right] == nums[right+1]){
+27                right--;
+28            }
+29            }
+30
+31        else if(sum < 0){
+32            left++;
+33        }
+34        else{
+35            right--;
+36        }
+37        }
+38        }
+39        return ans;
+40    }
+41};
