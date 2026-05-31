43b02a28efe6f6825d122c2e725abdc1f5f35f0b
Kapil <kydv901@gmail.com>
2026-05-31 12:12:17 +0530

    [26] [Time Beats: 100.00%] [Memory Beats: 51.19%] - LeetPush

--- Remove-Duplicates-from-Sorted-Array.cpp
+++ Remove-Duplicates-from-Sorted-Array.cpp
@@ -14,14 +14,22 @@
 14
 15
 16        //USING SET.
-17        set<int> s(nums.begin(),nums.end());
+17        // set<int> s(nums.begin(),nums.end());
 18
-19        int i=0;
-20        for(int x : s){
-21            nums[i] = x;
-22            i++;
-23        }
+19        // int i=0;
+20        // for(int x : s){
+21        //     nums[i] = x;
+22        //     i++;
+23        // }
 24
-25        return s.size();
-26    }
-27};
+25        // return s.size();
+26
+27
+28
+29
+30    //USING UNIQUE INBUILT LIBRARY.
+31    auto it = unique(nums.begin(),nums.end());
+32    nums.erase(it,nums.end());
+33    return nums.size();
+34    }
+35};
