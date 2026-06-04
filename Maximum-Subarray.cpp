ddbcd3d44f2678f9b8de5e2fc16c46ce1ed14de8
Kapil <kydv901@gmail.com>
2026-06-04 21:03:25 +0530

    [53] [Time Beats: 100.00%] [Memory Beats: 14.82%] - LeetPush

--- Maximum-Subarray.cpp
+++ Maximum-Subarray.cpp
@@ -1,12 +1,19 @@
 1class Solution {
 2public:
 3    int maxSubArray(vector<int>& nums) {
-4        int currsum = nums[0];
-5        int maxsum = nums[0];
-6        for(int i=1 ; i<nums.size() ; i++){
-7            currsum = max(nums[i],nums[i]+currsum);
-8            maxsum = max(currsum,maxsum);
-9        }
-10        return maxsum;
-11    }
-12};
+4        int n = nums.size();
+5        int maxsum = INT_MIN;
+6        int sum = 0;
+7        for(int i=0 ; i<n ; i++){
+8            sum += nums[i];
+9            if(sum > maxsum){
+10                maxsum = sum;
+11            }
+12            if(sum<0){
+13                sum=0;
+14            }
+15        }
+16        
+17        return maxsum;
+18    }
+19};
