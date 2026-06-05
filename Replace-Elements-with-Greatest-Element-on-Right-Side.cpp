4a453c3873a22a06f70fbadd1f8ba6056df8cb4f
Kapil <kydv901@gmail.com>
2026-06-05 21:57:23 +0530

    [1299] [Time Beats: 46.77%] [Memory Beats: 8.91%] - LeetPush

--- Replace-Elements-with-Greatest-Element-on-Right-Side.cpp
+++ Replace-Elements-with-Greatest-Element-on-Right-Side.cpp
@@ -1,15 +1,15 @@
 1class Solution {
 2public:
 3    vector<int> replaceElements(vector<int>& arr) {
-4        int n = arr.size();
-5        vector<int> ans;
-6        int maxval = -1;
-7        for(int i=n-1 ; i>=0 ; i--){
-8            ans.push_back(maxval);
-9            maxval = max(maxval,arr[i]);
-10        }
-11
-12        //bcz we have used push back , which pushes the maxelement of thelastto the fonrt.
+4        vector<int> ans;
+5        int n = arr.size();
+6        int maxelement = -1;
+7
+8        for(int i=n-1 ; i>=0 ; i--){
+9            ans.push_back(maxelement);
+10            maxelement = max(maxelement,arr[i]);
+11        }
+12
 13        reverse(ans.begin(),ans.end());
 14        return ans;
 15    }
