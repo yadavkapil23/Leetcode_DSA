39ad853b736a940b800c05884a84390f29e338c2
Kapil <kydv901@gmail.com>
2026-06-07 15:14:07 +0530

    [560] [Time Beats: 7.49%] [Memory Beats: 94.67%] - LeetPush

--- Subarray-Sum-Equals-K.cpp
+++ Subarray-Sum-Equals-K.cpp
@@ -1,18 +1,19 @@
 1class Solution {
 2public:
 3    int subarraySum(vector<int>& nums, int k) {
-4        int n=nums.size();
-5        int count=0;
+4        int n = nums.size();
+5        int count = 0;
 6        for(int i=0 ; i<n ; i++){
-7            int currsum = 0;
+7            int sum = 0;
 8            for(int j=i ; j<n ; j++){
-9                currsum += nums[j];
+9                sum += nums[j];
 10
-11            if(currsum == k){
-12               count++;
+11            if(sum == k){
+12                count++;
 13            }
 14            }
 15        }
-16        return count;
-17    }
-18};
+16
+17        return count;
+18    }
+19};
