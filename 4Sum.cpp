16a201a6a0d339c1c574bcb680c9b9d4cabed633
Kapil <kydv901@gmail.com>
2026-06-09 12:38:54 +0530

    [18] [Time Beats: 15.03%] [Memory Beats: 98.77%] - LeetPush

--- 4Sum.cpp
+++ 4Sum.cpp
@@ -1,41 +1,49 @@
-class Solution {
-public:
-    vector<vector<int>> fourSum(vector<int>& v, int target) {
-        vector<vector<int>> res;
-        if(v.size() < 4) 
-        return {};
-        sort(v.begin(),v.end());
-        for(int i=0 ; i<v.size()-3 ; i++){
-            if(i>0 && v[i] == v[i-1]){
-                continue;
-            }
-            for(int j=i+1 ; j<v.size()-2 ; j++){
-            if(j>i+1 && v[j] == v[j-1]){
-                continue;
-            }
-            int left = j+1;
-            int right = v.size()-1;
-            while(left < right){
-                long long sum = (long long)v[i] + (long long)v[j] + (long long)v[left] + (long long)v[right];
-
-                
-                if(sum == target){
-                    res.push_back({v[i],v[j],v[left],v[right]});
-                    while(left < right && left+1 < v.size() && v[left] == v[left+1]) left++;
-                        while(left < right && right-1 > j && v[right] == v[right-1]) right--;
-
-                    left++;
-                    right--;
-                }
-                else if(sum < target){
-                    left++;
-                }
-                else{
-                    right--;
-                }
-            }
-        }
-        }
-        return res;
-    }
-};
+1class Solution {
+2public:
+3    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+4        int n = nums.size();
+5
+6        vector<vector<int>> ans;
+7        sort(nums.begin(),nums.end());
+8
+9        for(int i=0 ; i<n-3 ; i++){
+10            if(i>0 && nums[i] == nums[i-1]){
+11                continue;
+12            }
+13        for(int j=i+1 ; j<n-2 ; j++){
+14            if(j>i+1 && nums[j] == nums[j-1]){
+15                continue;
+16            }
+17
+18        long long left = j+1;
+19        long long right = n-1;
+20
+21        while(left < right){
+22            long long sum = (long long)nums[i] + (long long)nums[j] + (long long)nums[left] + (long long)nums[right];
+23
+24            if(sum == target){
+25                ans.push_back({nums[i],nums[j],nums[left],nums[right]});
+26                left++;
+27                right--;
+28
+29            while(left <  right && nums[left] == nums[left-1]){
+30                left++;
+31            }
+32
+33            while(left < right && nums[right] == nums[right+1]){
+34                right--;
+35            }
+36            }
+37
+38           else if(sum < target){
+39                left++;
+40            }
+41            else{
+42                right--;
+43            }
+44        }
+45        }
+46        }
+47        return ans;
+48    }
+49};
