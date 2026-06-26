5a935834a695828e47419a42a064939854387f15
Kapil <kydv901@gmail.com>
2026-06-27 00:48:45 +0530

    [344] [Time Beats: 100.00%] [Memory Beats: 52.08%] - LeetPush

--- Reverse-String.cpp
+++ Reverse-String.cpp
@@ -2,6 +2,18 @@
 2public:
 3    void reverseString(vector<char>& s) {
 4
-5        reverse(s.begin(),s.end());
-6    }
-7};
+5        // reverse(s.begin(),s.end());
+6        int n = s.size();
+7
+8        int left = 0;
+9        int right = n-1;
+10
+11        while(left < right){
+12            char temp  = s[left];
+13            s[left] = s[right];
+14            s[right] = temp;
+15            left++;
+16            right--;
+17        }
+18    }
+19};
