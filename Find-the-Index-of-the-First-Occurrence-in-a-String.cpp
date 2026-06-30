f384bce6e67269e8813047cd4874365503183a09
Kapil <kydv901@gmail.com>
2026-06-30 09:46:10 +0530

    [28] [Time Beats: 100.00%] [Memory Beats: 45.63%] - LeetPush

--- Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
+++ Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
@@ -1,10 +1,12 @@
 1class Solution {
 2public:
 3    int strStr(string haystack, string needle) {
-4    int pos = haystack.find(needle);
-5    if(pos == string::npos){
-6        return -1;
-7    }
-8        return pos;
-9    }
-10};
+4        int pos = haystack.find(needle);
+5
+6        if(pos == string::npos){
+7            return -1;
+8        }
+9        
+10        return pos;
+11    }
+12};
