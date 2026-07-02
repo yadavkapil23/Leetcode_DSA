8e4db5dddc6cfc76b41cb482a12bb3267120b2e1
Kapil <kydv901@gmail.com>
2026-07-02 10:43:41 +0530

    [443] [Time Beats: 100.00%] [Memory Beats: 14.54%] - LeetPush

--- String-Compression.cpp
+++ String-Compression.cpp
@@ -1,34 +1,33 @@
 1class Solution {
 2public:
 3    int compress(vector<char>& chars) {
-4    
-5    int n = chars.size();
-6    int index = 0;
-7    char currentchar;
-8    
-9    int i=0;
-10    while(i<n){
-11        currentchar = chars[i];
-12        int count = 0;
-13        
-14        while(i<n && chars[i] ==  currentchar){
-15            count++;
-16            i++;
-17        }
-18        
-19        //assign work
-20        chars[index] = currentchar;
-21        index++;
-22        
-23        if(count>1){
-24            string count_str = to_string(count);
-25            for(char c : count_str){
-26                chars[index] = c;
-27                index++;
-28
-29            }
-30        }
-31    }
-32    return index;
-33    }
-34};
+4        
+5        int i=0;
+6        int index = 0;
+7        char currentchar;
+8        int n = chars.size();
+9        while(i<n){
+10            int count = 0;
+11
+12            currentchar = chars[i];
+13
+14            while(i<n && chars[i] == currentchar){
+15                count++;
+16                i++;
+17            }
+18
+19            chars[index] = currentchar;
+20            index++;
+21
+22            if(count > 1){
+23                string count_str = to_string(count);
+24                for(char c : count_str){
+25                    chars[index] =  c;
+26                    index++;
+27                }
+28            }
+29        }
+30
+31        return index;
+32    }
+33};
