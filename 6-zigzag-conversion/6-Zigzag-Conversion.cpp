2fe0ff77de706e4fdb9f664e91aabd4ba031eb53
Kapil <kydv901@gmail.com>
2026-07-06 18:29:35 +0530

    [6] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

---
+++ 6-zigzag-conversion/6-Zigzag-Conversion.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string convert(string s, int numRows) {
+        int n = s.size();
+        vector<string> rows(numRows);
+        if(n==1 || numRows > s.size()){
+            return s;
+        }
+        int currentrow = 0;
+        int direction = 1;
+        string ans;
+        for(char c : s){
+            rows[currentrow] += c;
+
+            if(currentrow == 0){
+                direction  = 1;
+            }
+
+            else if(currentrow == numRows - 1){
+                direction = -1;
+            }
+
+            currentrow = currentrow +  direction;
+        }
+
+        for(int i=0 ; i<rows.size() ; i++){
+            ans += rows[i];
+        }
+
+        return ans;
+    }
+};
