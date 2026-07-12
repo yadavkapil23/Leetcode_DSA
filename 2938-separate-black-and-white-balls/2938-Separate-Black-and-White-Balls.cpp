96d189110565452b963b74bfebd559678730d159
Kapil <kydv901@gmail.com>
2026-07-12 15:02:09 +0530

    [2938] [Time Beats: 61.61%] [Memory Beats: 41.54%] - LeetPush

---
+++ 2938-separate-black-and-white-balls/2938-Separate-Black-and-White-Balls.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long minimumSteps(string s) {
+        long long swap = 0;
+        long long black = 0;
+
+        for(int i=0 ; i<s.size() ; i++){
+            if(s[i] == '0'){
+                swap += black;
+            }
+            else{
+                black++;
+            }
+        }
+
+        return swap;
+    }
+};
