aee3d663520607b9cf48c43068d0532aaaa1e466
Kapil <kydv901@gmail.com>
2026-07-14 11:44:57 +0530

    [55] [Time Beats: 100.00%] [Memory Beats: 58.60%] - LeetPush

---
+++ 55-jump-game/55-Jump-Game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int maxreach = 0;
+        for(int i=0 ; i<n ; i++){
+            if(i>maxreach){
+                return false;
+            }
+
+            maxreach = max(maxreach,i+nums[i]);
+
+            if(maxreach >= n){
+                return true;
+            }
+        }
+
+        return true;
+    }
+};
