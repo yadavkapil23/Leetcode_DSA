5b7bde5dcf2b68ed018ad945d0fb6fec9426c17f
Kapil <kydv901@gmail.com>
2026-07-15 22:02:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (9.11%) - LeetSync

--- 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -3,11 +3,6 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumodd = n*n;
-        int sumeven = n*(n+1);
-
-        int x = std::gcd(sumodd,sumeven);
-
-        return x;
+        return n;
     }
 };
