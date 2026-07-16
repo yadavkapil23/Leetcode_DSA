e29549872da6844bc09b00bbe527f3508d934432
Kapil <kydv901@gmail.com>
2026-07-16 21:58:10 +0530

    [496] [Time Beats: 10.69%] [Memory Beats: 80.42%] - LeetPush

--- 496-next-greater-element-i/496-Next-Greater-Element-I.cpp
+++ 496-next-greater-element-i/496-Next-Greater-Element-I.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         int n = nums2.size();
-        vector<int> v(n);
-        stack<int>  st;
+        int m = nums1.size();
+        stack<int> st;
+        vector<int> ans(n);
         for(int i=n-1 ; i>=0 ; i--){
+            
             while(!st.empty() && st.top() <= nums2[i]){
                 st.pop();
             }
 
             if(st.empty()){
-                v[i] = -1;
+                ans[i] = -1;
             }
 
             else{
-                v[i] = st.top();
+                ans[i] = st.top();
             }
 
             st.push(nums2[i]);
         }
 
-
-        vector<int> result;
+       vector<int> result;
         for(auto x : nums1){
             for(int j=0 ; j<nums2.size() ; j++){
                 if(nums2[j] == x){
-                    result.push_back(v[j]);
+                    result.push_back(ans[j]);
                 }
             }
         }
