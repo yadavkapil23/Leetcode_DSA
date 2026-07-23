e5b7f5d5456556048aa85de2176b00f01d816839
Kapil <kydv901@gmail.com>
2026-07-23 09:49:35 +0530

    [39] [Time Beats: 100.00%] [Memory Beats: 59.15%] - LeetPush

---
+++ 39-combination-sum/39-Combination-Sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void backtrack(int sum,int index,vector<int> & candidates,int target,vector<int> &curr, vector<vector<int>> &ans){
+        if(index==candidates.size()){
+             ans.push_back(curr);
+            return;
+        }
+        if (target == 0) {
+        ans.push_back(curr);
+          return;
+         }
+
+        if (target < 0) {
+         return;
+        }
+
+
+        for(int i=index ; i<candidates.size(); i++){
+            curr.push_back(candidates[i]);
+            backtrack(sum,i,candidates,target-candidates[i],curr,ans);
+            curr.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<int> curr;
+        vector<vector<int>> ans;
+        backtrack(0,0,candidates,target, curr,ans);
+        return ans;
+    }
+};
