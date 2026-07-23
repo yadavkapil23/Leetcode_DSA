d0efb77d739dbe251abf2b842d6aa14e999c6339
Kapil <kydv901@gmail.com>
2026-07-23 14:58:42 +0530

    Time: 207 ms (65.70%) | Memory: 10.6 MB (65.08%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,44 +1,32 @@
 class Solution {
 public:
-    int l, m, n;
-    vector<vector<int>> directions{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
-    bool find(vector<vector<char>>& board, int i, int j, string &word, int idx) {
-        if(idx >= l)
-            return true;
-        
-        if(i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[idx])
+    bool dfs(vector<vector<char>>& board, string &word, int i, int j, int k) {
+        if (k == word.size()) return true;
+
+        if (i < 0 || j < 0 || i >= board.size() || j >= board[0].size() ||
+            board[i][j] != word[k])
             return false;
-        
-        char temp = board[i][j];
-        board[i][j] = '$';
-        
-        for(auto& dir : directions) {
-            int i_ = i + dir[0];
-            int j_ = j + dir[1];
-            
-            if(find(board, i_, j_, word, idx+1))
-                return true;
-        }
-        
-        board[i][j] = temp;
-        return false;
+
+        char ch = board[i][j];
+        board[i][j] = '#';
+
+        bool found =
+            dfs(board, word, i + 1, j, k + 1) ||
+            dfs(board, word, i - 1, j, k + 1) ||
+            dfs(board, word, i, j + 1, k + 1) ||
+            dfs(board, word, i, j - 1, k + 1);
+
+        board[i][j] = ch;
+        return found;
     }
-    
+
     bool exist(vector<vector<char>>& board, string word) {
-        m = board.size();
-        n = board[0].size();
-        l = word.length();
-        if(m*n < l)
-            return false;
-        
-        for(int i = 0; i<m; i++) {
-            for(int j = 0; j<n; j++) {
-                if(board[i][j] == word[0] && find(board, i, j, word, 0)) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                if (dfs(board, word, i, j, 0))
                     return true;
-                }
             }
         }
-        
         return false;
     }
 };
