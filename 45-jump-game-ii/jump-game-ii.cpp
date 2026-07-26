ec12a8d390d832b43765821fccb7817fa1fbe614
Kapil <kydv901@gmail.com>
2026-07-26 22:23:56 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (71.84%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -6,10 +6,10 @@ public:
         int farthest = 0;
         int jump=0;
 
-        for(int i=0 ; i<n-1 ; i++){
-            farthest = max(farthest,i+nums[i]);
+        for(int i=0 ; i<n-1 ; i++){ //skip the last jump , as we have reached the destination.
+            farthest = max(farthest,i+nums[i]); //farthese point we can go from this position.
 
-            if(i==currend){
+            if(i==currend){ //if the ith pointer is at the prev jump ending point, menas , now we need one more jump to go next , so jump++ and now initialse the currend to farthest.
                 jump++;
                 currend = farthest;
             }
