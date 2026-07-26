2547faef3b857da627a085d1710483945643ad55
Kapil <kydv901@gmail.com>
2026-07-26 12:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (30.77%) - LeetSync

---
+++ 62-unique-paths/unique-paths.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int func(int m,int n,vector<vector<int>> &dp){
+        if(m==0 && n==0){
+            return 1;
+        }
+        if(m<0 || n<0){
+            return 0;
+        }
+
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+
+        int up = func(m-1,n,dp);
+        int left = func(m,n-1,dp);
+
+       return dp[m][n] = up+left;
+    }
+    int uniquePaths(int m, int n) {
+       vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+       return func(m-1,n-1,dp);
+    }
+};
