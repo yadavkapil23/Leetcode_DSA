e55bc577051b21784165005ef4540522db3e865b
Kapil <kydv901@gmail.com>
2026-07-30 11:01:49 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (5.39%) - LeetSync

---
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+vector<int> ans;
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root == nullptr){
+            return ans;
+        }
+
+        inorderTraversal(root->left);
+        ans.push_back(root->val);
+        inorderTraversal(root->right);
+
+
+        return ans;
+    }
+};
