628f87110b577ae3f70a0b8995fb66278fec83b4
Kapil <kydv901@gmail.com>
2026-09-07 13:54:33 +0530

    [2114] [Time Beats: 5.25%] [Memory Beats: 5.43%] - LeetPush

---
+++ 2114-Maximum-Number-of-Words-Found-in-Sentences.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int counter(string &s){
+        int count = 0;
+        stringstream ss(s);
+        string word;
+
+        while(ss >> word){
+            count++;
+        }
+
+        return count;
+    }
+    int mostWordsFound(vector<string>& sentences) {
+        int ans = 0;
+        for(int i=0 ; i<sentences.size() ; i++){
+            string s = sentences[i];
+            int freq = counter(s);
+            ans = max(freq,ans);
+        }
+        return ans;
+    }
+};
