43f981f57c513748b89d6756ac2162fcaee7223f
Kapil <kydv901@gmail.com>
2026-09-11 22:41:12 +0530

    Time: 4 ms (43.16%) | Memory: 146.7 MB (91.86%) - LeetSync

---
+++ 111-minimum-depth-of-binary-tree/minimum-depth-of-binary-tree.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root == nullptr){
+            return 0;
+        }
+
+       if(!root->left){
+       return 1+ minDepth(root->right);
+       }
+       if(!root->right){
+        return 1+minDepth(root->left);
+       }
+
+      return 1 + min(minDepth(root->left), minDepth(root->right));
+    }
+};
